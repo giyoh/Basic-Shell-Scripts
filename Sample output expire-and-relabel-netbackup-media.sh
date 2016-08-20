2be0a4a2225b9ef4fc3b6b20b1a965de87b9c99f
Emmanuel Giyoh Ngala <giyoh@programmer.net>
2016-08-20 18:31:55 +0100

    Create Sample output expire-and-relabel-netbackup-media.sh

---
+++ Sample output expire-and-relabel-netbackup-media.sh
@@ -0,0 +1,36 @@
+################################################################################################
+#
+#
+# SAMPLE OUTPUT
+#
+#
+###############################################################################################
+
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP020 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP047 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP057 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP063 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP130 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP150 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP202 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP221 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP228 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP234 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP928 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP929 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP932 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP933 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP934 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP937 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP951 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP963 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP965 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP973 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP974 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP976 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP977 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP978 -d HCART3 -o -p Vault_Pool 
+/usr/openv/netbackup/bin/admincmd/bplabel -m V00004 -d DLT -o -p vtl-test 
+/usr/openv/netbackup/bin/admincmd/bplabel -m V00010 -d DLT -o -p vtl-test 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP075 -d HCART3 -o -p BurjFileServer_Full 
+/usr/openv/netbackup/bin/admincmd/bplabel -m EBP083 -d HCART3 -o -p BurjFileServer_Full
